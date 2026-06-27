fad31b8d6eb6386c0ee1d47392b51c042547a12c
ankio <ankio@ankio.net>
2026-06-27 16:16:35 +0800

    :recycle: (core): 更新HTTP客户端请求头配置

--- HttpClient.php
+++ HttpClient.php
@@ -67,7 +67,6 @@ class HttpClient
             'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
             'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'Accept-Language' => 'zh-CN,zh;q=0.9,en;q=0.8',
-            'Accept-Encoding' => 'gzip, deflate, br',
             'Connection' => 'keep-alive',
             'Upgrade-Insecure-Requests' => '1',
             'Sec-Fetch-Dest' => 'document',
